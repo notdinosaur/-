c5400a8d0596ab17f81cba300a3618c9f96bea80
notdinosaur <159621721+notdinosaur@users.noreply.github.com>
2024-02-13 23:18:45 +0800

    Update control.h

--- control.h
+++ control.h
@@ -17,33 +17,17 @@ typedef struct
 
 typedef struct
 {
-    float Kp;
-    float Ki;
-    float vo;
-    float target,actual;
-} PI;
-
-typedef struct
-{
-    float Ui1,Ui2;
-    float Ualpha, Ubeta;
-    float Ud, Uq;
-    float sinwt, coswt;
-    float ref, fed, err, errpre, tempvalue, out;
-    float Kp,Ki;
-    float TS;
-} PLL;
-
-typedef struct 
-{
     
-} coordinate_transformation;
+
+} SOGI;
+
+
+
+
 
 
 void PR_init(PR *pr,float Kp,float Kr,float Ts,float wc, float wo,float input);
 float PR_calc(PR *pr,float target,float actual);
-void Phase_PLL(float U1,float U2,float Kp,float Ki,float Ts);
-//void PI_init(PI *pi,float Kp,float Ki);
-//float PI_calc(PI *pi,float target,float actual);
+
 
 #endif /* CONTROL_H_ */
